52fa806cab82e93b6df09a4629b357f5ee413542
luyong6 <luyong@cn.ibm.com>
2019-07-13 08:28:58 +0200

    improve the comments for VPD code gen
    Signed-off-by: luyong6 <luyong@cn.ibm.com>

--- common/script/gen_vsec.sh
+++ common/script/gen_vsec.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-#This script helps you generate the vpd data (vpd44data_be) used in capi_vsec.vhdl
+#This script helps you generate the VPD (Virtual Product Data) (vpd44data_be) used in capi_vsec.vhdl
 #The original script comes from https://github.com/open-power/capiflash/blob/master/src/afu/surelock_vpd2rbf 
 
 # TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
-# TODO 1. Update Product information in vpd_CARDNAME.csv, use vpd_FlashGT.csv as an example TODO
-# TODO 2. Modify the CompanyName and CardName on line 20 !!!                                TODO
+# TODO                                                                                      TODO
+# TODO 1. Write information in new file vpd_CARDNAME.csv, use vpd_FlashGT.csv as template   TODO
+# TODO 2. Modify the company_name and card_name!!!                                          TODO
 # TODO 3. Run this script "./gen_vsec.sh vpd_CARDNAME.csv"                                  TODO
 # TODO 4. Copy/Paste the generated lines into capi_vsec.vhdl                                TODO
+# TODO                                                                                      TODO
 # TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO 
 
 company_name="ThisCompany"
