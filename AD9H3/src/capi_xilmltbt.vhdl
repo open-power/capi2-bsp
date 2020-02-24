446ff0103e246cd2923effcac04154f633ae8a55
Bruno Mesnet <bruno.mesnet@fr.ibm.com>
2020-02-24 11:02:36 +0100

    correct multiboot
    Signed-off-by: Bruno Mesnet <bruno.mesnet@fr.ibm.com>

--- AD9H3/src/capi_xilmltbt.vhdl
+++ AD9H3/src/capi_xilmltbt.vhdl
@@ -90,7 +90,6 @@ signal icap_request_q: std_logic;
 signal icap_request_fall: std_logic;
 signal icap_mltbt_takeover_d: std_logic;
 signal icap_mltbt_takeover_q: std_logic;
-
 begin
 
     dff_start: capi_rise_dff PORT MAP (
@@ -130,10 +129,10 @@ begin
     --  wbstart_addr <= ( "000" & "00" & cpld_user_bs_req & "00" & "000000000000000000000000" );
 
     -- setting 0x100_0000 as User space address (default for AD9V3 card)
-    -- wbstart_addr <= ( "000" & "0000" & cpld_user_bs_req & "000000000000000000000000" );
+    --wbstart_addr <= ( "000" & "0000" & cpld_user_bs_req_inv & "000000000000000000000000" );
 
    -- setting 0x400_0000 as User space address (default for AD9H3 card)
-    wbstart_addr <= ( "000" & "00" & cpld_user_bs_req & "00000000000000000000000000" );
+    wbstart_addr <= ( "000" & "0000" & cpld_user_bs_req & "000000000000000000000000" );
 
 
 
